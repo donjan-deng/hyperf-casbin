36826dc64f2a313d3b62989725c03493f9427a1a
Donjan <Donjan@cqbaobao.cn>
2021-08-02 11:38:50 +0800

    actions

--- .github/scripts/setup.mysql.sh
+++ .github/scripts/setup.mysql.sh
@@ -4,7 +4,7 @@ sudo /etc/init.d/mysql start
 echo -e "Config Mysql..."
 mysql -h 127.0.0.1 -uroot -proot -e "use mysql;update user set plugin='mysql_native_password',authentication_string='' where User='root';FLUSH PRIVILEGES;"
 echo -e "Create MySQL database..."
-mysql -h 127.0.0.1 -uroot -e "CREATE DATABASE IF NOT EXISTS casbin-test charset=utf8mb4 collate=utf8mb4_unicode_ci;"
+mysql -h 127.0.0.1 -uroot -e "CREATE DATABASE IF NOT EXISTS casbin_test charset=utf8mb4 collate=utf8mb4_unicode_ci;"
 echo -e "Done\n"
 
 wait
