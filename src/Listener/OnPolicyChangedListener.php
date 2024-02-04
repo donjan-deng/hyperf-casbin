d106ae46edb2d03af5002fa44e0ab3cd2837b66d
Donjan <8256490+donjan-deng@users.noreply.github.com>
2024-02-04 17:25:02 +0800

    #36

--- src/Listener/OnPolicyChangedListener.php
+++ src/Listener/OnPolicyChangedListener.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Donjan\Casbin\Listener;
 
-use Hyperf\Contract\ConfigInterface;
 use Psr\Container\ContainerInterface;
 use Hyperf\Event\Contract\ListenerInterface;
 use Hyperf\Contract\ConfigInterface;
