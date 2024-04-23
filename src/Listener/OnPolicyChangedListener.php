521d48c1dc2fadfd7398ed5f5fd994c33d476063
Donjan <8256490+donjan-deng@users.noreply.github.com>
2024-04-23 17:30:26 +0800

    fix:https://github.com/donjan-deng/hyperf-casbin/issues/37

--- src/Listener/OnPolicyChangedListener.php
+++ src/Listener/OnPolicyChangedListener.php
@@ -4,14 +4,15 @@ declare(strict_types=1);
 
 namespace Donjan\Casbin\Listener;
 
+use Hyperf\Contract\ConfigInterface;
 use Psr\Container\ContainerInterface;
 use Hyperf\Event\Contract\ListenerInterface;
-use Hyperf\Contract\ConfigInterface;
 use Donjan\Casbin\Event\PolicyChanged;
 use Donjan\Casbin\Event\PipeMessage;
 use Swoole\Server;
 use Hyperf\Process\ProcessCollector;
 use Hyperf\Server\ServerManager;
+use Hyperf\Server\CoroutineServer;
 
 class OnPolicyChangedListener implements ListenerInterface
 {
@@ -20,12 +21,10 @@ class OnPolicyChangedListener implements ListenerInterface
      * @var ContainerInterface
      */
     private $container;
-    private $config;
 
     public function __construct(ContainerInterface $container)
     {
         $this->container = $container;
-        $this->config = $this->container->get(ConfigInterface::class);
     }
 
     public function listen(): array
@@ -37,16 +36,16 @@ class OnPolicyChangedListener implements ListenerInterface
 
     public function process(object $event): void
     {
-        if ($this->config->get('casbin.watcher.enabled')) { //启用watcher，不响应此事件
+        if (config('casbin.watcher.enabled')) { //启用watcher，不响应此事件
             return;
         }
         $serverManager = $this->container->get(ServerManager::class);
         if (count($serverManager::list()) > 0 && $event instanceof PolicyChanged) {
             $serverConfig = $this->container->get(ConfigInterface::class)->get('server', []);
-            if (! $serverConfig) {
+            if (!$serverConfig) {
                 throw new \InvalidArgumentException('At least one server should be defined.');
             }
-            if ($serverConfig['type'] == Server::class) {
+            if (isset($serverConfig['type']) == false || (isset($serverConfig['type']) && $serverConfig['type'] != CoroutineServer::class)) { //Hyperf\Server\CoroutineServer 协程风格
                 $server = $this->container->get(Server::class);
                 $workerCount = $server->setting['worker_num'] + ($server->setting['task_worker_num'] ?? 0) - 1;
                 if ($workerCount > 0) {
